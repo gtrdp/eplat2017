cf006d3e5d6094f659c4d9f1a91b99cae198f4b8
Guntur DP <guntur.dharma@gmail.com>
2017-05-26 15:49:39 +0700

    new android project

---
+++ Eplat/app/src/main/java/ugm/dteti/se/eplat/eplat/MainActivity.java
@@ -0,0 +1,13 @@
+package ugm.dteti.se.eplat.eplat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
