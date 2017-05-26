e2aa7bf1d5d7fbc7842c0fbfe77c294b40305517
Guntur DP <guntur.dharma@gmail.com>
2017-05-26 16:25:04 +0700

    hello world flask

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello world'
+    return 'Hello world! Eplat 2017!'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
