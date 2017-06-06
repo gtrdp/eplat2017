7aeaf00376e6709e25ad79aba35f81f8ead5179a
Guntur DP <guntur.dharma@gmail.com>
2017-06-06 11:55:20 +0700

    working android code to send json data

--- Eplat/app/src/main/java/ugm/dteti/se/eplat/eplat/MainActivity.java
+++ Eplat/app/src/main/java/ugm/dteti/se/eplat/eplat/MainActivity.java
@@ -1,37 +1,101 @@
 package ugm.dteti.se.eplat.eplat;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.gson.GsonBuilder;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-
-    MapView mapView;
-    GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button btnStart = (Button) findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Sending the data!", Toast.LENGTH_SHORT).show();
 
-        // Gets the MapView from the XML layout and creates it
-        mapView = (MapView) findViewById(R.id.mapview);
-        mapView.onCreate(savedInstanceState);
+                new PostJSON("http://10.42.10.248:5000").execute();
+            }
+        });
+    }
+}
 
-        // Gets to GoogleMap from the MapView and does initialization stuff
-        map = mapView.getMap();
-        map.getUiSettings().setMyLocationButtonEnabled(false);
-        map.setMyLocationEnabled(true);
+class PostJSON extends AsyncTask<String, Void, String> {
 
-        // Needs to call MapsInitializer before doing any CameraUpdateFactory calls
+    private String uri;
+
+    public PostJSON(String u) {
+        uri = u;
+    }
+
+    @Override
+    protected String doInBackground(String... arg0) {
+        // TODO Auto-generated method stub
+        return execute();
+    }
+
+    private String execute() {
+        // creates a JSON object with the below
+        Map<String, String> vehicle = new HashMap<>();
+        vehicle.put("id", "12345");
+        vehicle.put("lat", "-7.797068");
+        vehicle.put("long", "110.370529");
+        vehicle.put("dir", "S");
+
+        String json = new GsonBuilder().create().toJson(vehicle, Map.class);
+
+        Log.v("JSON Result", json);
+        return makeRequest(uri, json).toString();
+    }
+
+    public String makeRequest(String uri, String json) {
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpPost httpPost = new HttpPost(uri);
         try {
-            MapsInitializer.initialize(this.getActivity());
-        } catch (GooglePlayServicesNotAvailableException e) {
+            //HttpPost httpPost = new HttpPost(uri);
+            httpPost.setEntity(new StringEntity(json));
+            httpPost.setHeader("Accept", "application/json");
+            httpPost.setHeader("Content-type", "application/json");
+            System.out.println("Call successful");
+            //return new DefaultHttpClient().execute(httpPost);
+            // Execute HTTP Post Request
+            ResponseHandler<String> responseHandler=new BasicResponseHandler();
+            return httpclient.execute(httpPost, responseHandler);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
+        return null;
+    }
 
-        // Updates the location and zoom of the MapView
-        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(43.1, -87.9), 10);
-        map.animateCamera(cameraUpdate);
+    @Override
+    protected void onPostExecute(String result) {
+        //Call the intent and pass the id from the http post response
+        System.out.println("Result: " + result);
     }
 }
---
+++ LogBook/02-june.md
@@ -0,0 +1,14 @@
+E-Plat LogBook May 2017
+=======================
+
+June #1
+-------
+- **Sen Jun  5 08:52:08 WIB 2017**
+  - Python for android.
+- **Sel Jun  6 08:52:08 WIB 2017**
+  - Working on the app: trying to send JSON data to server.
+  - https://github.com/googlemaps/android-maps-utils
+  - https://stackoverflow.com/questions/36833798/sending-json-object-via-http-post-method-in-android
+  - http://osamashabrez.com/client-server-communication-android-json/
+  - https://gist.github.com/sslover/5144022
+  - Got RFID Reader Omnikey.
